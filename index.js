13d2687a5ea092b848429fa2d9328e0135da3a52
winarwahyuw <winarwahyuw99@gmail.com>
2022-06-04 18:15:44 +0700

    done

--- index.js
+++ index.js
@@ -6,15 +6,24 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (data, sort) => {
+  const sortData = sort(data);
+  return sortData.map( (data, index) => {
+    return index+1+". "+data;
+  });
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data) => {
+  return data.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data) => {
+  return data.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
